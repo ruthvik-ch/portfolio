2b87b89fdb8354b4af620885488e5a0646a461e6
ruthvik-ch <chennapragadaruthvik@gmail.com>
2025-08-04 10:52:27 +0530

    use prod envi and enableprodmode()

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app.component';
+import { enableProdMode } from '@angular/core';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
