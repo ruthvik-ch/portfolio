2ac9c8f47222bd6d6daa5bec57069ff8c3007b01
ruthvik-ch <chennapragadaruthvik@gmail.com>
2025-08-04 11:49:35 +0530

    performace increase by CLS reduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,29 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { LoaderComponent } from './shared/components/loader/loader.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    FooterComponent,
+    LoaderComponent,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class App {
   protected title = 'ruthvik-portfolio-website';
+
+  isLoading = true;
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 0);
+  }
 }
